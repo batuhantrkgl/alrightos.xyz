28ee3bee5cea1990e39a3e7f5b41b82abb0c3ac6
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2023-03-14 20:29:30 +0300

    Update getLatestVersion.js

--- assets/js/getLatestVersion.js
+++ assets/js/getLatestVersion.js
@@ -1,6 +1,6 @@
 function getLatestVersion() {    
     var xmlHttp = new XMLHttpRequest();
-    var requestLink = "https://api.github.com/repos/exteraSquad/exteraGram/releases/latest";
+    var requestLink = "https://api.github.com/repos/AlrightAndroid/updates/releases/latest";
     xmlHttp.open("GET", requestLink, false);
     xmlHttp.send(null);
     var releaseInfo = JSON.parse(xmlHttp.responseText);
